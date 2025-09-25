31a1269a84724b90afe06321a4f8da397f52f228
Lendariow <kiiwl.tell@gmail.com>
2025-09-25 18:42:42 -0300

    Create GameState.java

---
+++ src/main/java/dev/enderman/minecraft/plugins/games/bedwars/enums/GameState.java
@@ -0,0 +1,20 @@
+package dev.enderman.minecraft.plugins.games.bedwars.enums;
+
+import org.bukkit.ChatColor;
+import org.jetbrains.annotations.NotNull;
+
+public enum GameState {
+	WAITING(ChatColor.GREEN + "Waiting for players..."),
+	COUNTDOWN(ChatColor.YELLOW + "Game starting!"),
+	PLAYING(ChatColor.RED + "Game active.");
+
+	private final String name;
+
+	GameState(@NotNull final String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
